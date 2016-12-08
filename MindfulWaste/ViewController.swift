a864d66cb459dda9c2cb08f03f179913b299208a
Alex Drewno <student@adrewno1.local>
2016-12-08 09:31:46 -0600

    Added Project

---
+++ MindfulWaste/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MindfulWaste
+//
+//  Created by adrewno1 on 12/6/16.
+//  Copyright © 2016 adrewno1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MindfulWaste' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Firebase/Core'
+
+  # Pods for MindfulWaste
+
+  target 'MindfulWasteTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MindfulWasteUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
