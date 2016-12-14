d709428f5ad255bd6c5d8a761641e6e0d903c924
Alex Drewno <student@adrewno1.local>
2016-12-14 09:47:13 -0600

    Login with dim function

--- MindfulWaste/MenuController.swift
+++ MindfulWaste/MenuController.swift
@@ -3,7 +3,11 @@ import SideMenuController
 
 class MenuController: UITableViewController {
     
+    enum Direction{ case In, Out }
     
+    
+    
+    //reports, new report, organization(s)?
     let array = ["Cell number 1", "Cell number 2", "Cell number 3"]
     override func viewDidLoad()
     {
@@ -11,7 +15,6 @@ class MenuController: UITableViewController {
         self.tableView.backgroundView = imageView
         tableView.separatorColor = UIColor.black
         tableView.tableFooterView = UIView(frame: CGRect.zero)
-
     }
     
   
@@ -68,7 +71,55 @@ class MenuController: UITableViewController {
             return 3
         }
     }
+
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        
+        if indexPath.section == 0 && indexPath.row == 0
+        {
+            performSegue(withIdentifier: "showLogin", sender: nil)
+            dim(direction: .In, alpha: 0.5, speed: 0.5)
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showLogin"
+        {
+            dim(direction: .In, alpha: 0.5, speed: 0.5)
+            print("called")
+        }
+    }
+    
+    @IBAction func unwindFromSecondary()
+    {
+ 
+    }
+    
+    func dim(direction: Direction, alpha: CGFloat, speed: CGFloat)
+    {
+        let dimView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        dimView.backgroundColor = UIColor.gray
+        dimView.alpha = 0.0
+        switch direction{
+            case .In:
+                self.view.addSubview(dimView)
+                dimView.alpha = 0.0
+                UIView.animate(withDuration: TimeInterval(speed), animations: { 
+                    dimView.alpha = alpha
+                })
+            case .Out:
+                dimView.alpha = alpha
+                UIView.animate(withDuration: TimeInterval(speed), animations: {
+                    dimView.alpha = 0.0
+                }, completion: { (bool:Bool) in
+                    dimView.removeFromSuperview()
+                })
+            
+        }
+        
+    }
     
     
 }
